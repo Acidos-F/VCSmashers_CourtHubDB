cd6eb051efb9b64ff83b89b788e47131fe199291
Acidos-F <71970335+Acidos-F@users.noreply.github.com>
2025-10-23 17:07:36 +0800

    added Bookings Table

--- VCSmashers_CourtHubDB.sql
+++ VCSmashers_CourtHubDB.sql
@@ -27,3 +27,25 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`),
   CONSTRAINT `fk_users_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ==============================
+-- Bookings
+-- ==============================
+CREATE TABLE IF NOT EXISTS `bookings` (
+  `booking_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT UNSIGNED NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `description` TEXT,
+  `activity_name` VARCHAR(255) NOT NULL, -- Pickleball / Badminton
+  `num_of_participants` INT NOT NULL,
+  `fee_per_head` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  `total_fee` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  `event_date` DATE NOT NULL,
+  `event_time` TIME NOT NULL,
+  `booking_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` VARCHAR(50) DEFAULT 'PENDING', -- PENDING, CONFIRMED, CANCELLED
+  PRIMARY KEY (`booking_id`),
+  CONSTRAINT `fk_bookings_user` FOREIGN KEY (`user_id`) 
+    REFERENCES `users`(`user_id`) 
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
