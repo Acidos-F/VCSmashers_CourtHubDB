6a2555487817ce14189782baa6664b173c603742
Acidos-F <71970335+Acidos-F@users.noreply.github.com>
2025-10-23 17:08:42 +0800

    added sample data for bookings table

--- Sample_Data.sql
+++ Sample_Data.sql
@@ -4,3 +4,7 @@ INSERT INTO users (role_id, name, email, phone, pass) VALUES
 (1, 'John Admin', 'john@gmail.com', '09986418731', '$2y$10$5J3cO1YKlG8mZywJkrpHMOEqW5MD4i.Nlo7P4Blv4VKyqGPrd160u'), -- admin123
 (2, 'Ben Santos', 'ben@example.com', '09171234567', '$2y$10$kY0gkkk.wakJh4o3bFxjnukq4DqkgqRn08ONW5CMYp0pi5L7SV8/G'), -- user123
 (2, 'Cara Reyes', 'cara@example.com', '09181234567', '$2y$10$kY0gkkk.wakJh4o3bFxjnukq4DqkgqRn08ONW5CMYp0pi5L7SV8/G'); -- user123
+
+-- Sample Data in bookings Table
+INSERT INTO bookings (user_id, title, description, activity_name, num_of_participants, fee_per_head, total_fee, event_date, event_time, status)
+VALUES (1, 'Morning Pickleball', 'Friendly match with friends', 'Pickleball', 5, 200.00, 1000.00, '2025-10-12', '09:00:00', 'CONFIRMED');
