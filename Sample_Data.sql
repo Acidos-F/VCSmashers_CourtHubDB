384921771878a4e136cec768fe80f13f8b9e191d
Acidos-F <71970335+Acidos-F@users.noreply.github.com>
2025-10-28 16:22:08 +0800

    Update Sample_Data.sql
    Added Sample Data

--- Sample_Data.sql
+++ Sample_Data.sql
@@ -5,6 +5,8 @@ INSERT INTO users (role_id, name, email, phone, pass) VALUES
 (2, 'Ben Santos', 'ben@example.com', '09171234567', '$2y$10$kY0gkkk.wakJh4o3bFxjnukq4DqkgqRn08ONW5CMYp0pi5L7SV8/G'), -- user123
 (2, 'Cara Reyes', 'cara@example.com', '09181234567', '$2y$10$kY0gkkk.wakJh4o3bFxjnukq4DqkgqRn08ONW5CMYp0pi5L7SV8/G'); -- user123
 
--- Sample Data in bookings Table
-INSERT INTO bookings (user_id, title, description, activity_name, num_of_participants, fee_per_head, total_fee, event_date, event_time, status)
-VALUES (1, 'Morning Pickleball', 'Friendly match with friends', 'Pickleball', 5, 200.00, 1000.00, '2025-10-12', '09:00:00', 'CONFIRMED');
+INSERT INTO bookings (user_id, title, description, activity_name, court_number, num_of_participants, fee_per_head, total_fee, event_date, event_time, status)
+VALUES (1, 'Morning Pickleball', 'Friendly match with friends', 'Pickleball', 1, 5, 200.00, 1000.00, '2025-10-12', '09:00:00', 'CONFIRMED');
+
+INSERT INTO tournaments (tournament_name, description, start_datetime, end_date, max_participants, entry_fee, status)
+VALUES ('Valenzuela Pickleball Cup', 'Citywide Tournament', '2025-11-01 09:00:00', '2025-11-03', 32, 500.00, 'SCHEDULED');
